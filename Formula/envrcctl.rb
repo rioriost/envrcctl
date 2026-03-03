c5c74700eca4d4bd63602557ea7080b8c42dc6a8
Rio Fujita <rio_github@rio.st>
2026-03-03 14:56:11 +0900

    infra: pin Homebrew typer resource to 0.24.1

--- Formula/envrcctl.rb
+++ Formula/envrcctl.rb
@@ -10,8 +10,8 @@ class Envrcctl < Formula
   depends_on "python@3.14"
 
   resource "typer" do
-    url "REPLACE_WITH_TYPER_URL"
-    sha256 "REPLACE_WITH_TYPER_SHA256"
+    url "https://files.pythonhosted.org/packages/f5/24/cb09efec5cc954f7f9b930bf8279447d24618bb6758d4f6adf2574c41780/typer-0.24.1.tar.gz"
+    sha256 "e39b4732d65fbdcde189ae76cf7cd48aeae72919dea1fdfc16593be016256b45"
   end
 
   def install
