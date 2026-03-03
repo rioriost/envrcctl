b5cf21220baa1d4d8bbed74be5ac354bdc421ac4
Rio Fujita <rio_github@rio.st>
2026-03-03 14:55:03 +0900

    infra: update Homebrew formula metadata and deps

--- Formula/envrcctl.rb
+++ Formula/envrcctl.rb
@@ -2,12 +2,17 @@ class Envrcctl < Formula
   include Language::Python::Virtualenv
 
   desc "Secure, structured management of .envrc files"
-  homepage "REPLACE_WITH_HOMEPAGE"
+  homepage "https://github.com/rioriost/homebrew-envrcctl"
   url "REPLACE_WITH_RELEASE_TARBALL_URL"
   sha256 "REPLACE_WITH_SHA256"
-  license "NOASSERTION"
+  license "MIT"
 
   depends_on "python@3.14"
+
+  resource "typer" do
+    url "REPLACE_WITH_TYPER_URL"
+    sha256 "REPLACE_WITH_TYPER_SHA256"
+  end
 
   def install
     virtualenv_install_with_resources
